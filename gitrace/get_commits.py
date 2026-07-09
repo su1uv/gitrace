757d504c94c72f4c79d7a6098d8a0570eb8b476b
su1uv <ortizemilio.dev@gmail.com>
2026-07-09 13:22:20 -0600

    [feat]: get commits function

--- gitrace/get_commits.py
+++ gitrace/get_commits.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-def get_commits(work_dir: str, file_path: str):
+def get_commits(work_dir: str, file_path: str) -> dict[str, tuple[str, str, str]]:
     get_commits: subprocess.CompletedProcess = subprocess.run(
         ("git", "-C", work_dir, "log", "--follow", file_path),
         capture_output=True,
@@ -9,7 +9,21 @@ def get_commits(work_dir: str, file_path: str):
     )
     commits_list: list[str] = []
     if isinstance(get_commits.stdout, str):
-        commits_list = get_commits.stdout.split("commit ")
+        commits_list = get_commits.stdout.replace("\n\n", "\n").split("commit ")
+    else:
+        exit(1)
+    commits_dict: dict[str, tuple[str, str, str]] = {}
 
     for commit in commits_list:
-        print(commit)
+        if commit == "":
+            continue
+        commit_content: list[str] = commit.splitlines()
+        commit_id: str = commit_content[0].strip()
+        commit_author: str = commit_content[1].strip()
+        commit_date: str = commit_content[2].strip()
+        commit_msg: str = commit_content[3].strip()
+
+        commit_data: tuple[str, str, str] = (commit_author, commit_date, commit_msg)
+        commits_dict[commit_id] = commit_data
+
+    return commits_dict
--- gitrace/main.py
+++ gitrace/main.py
@@ -1,13 +1,16 @@
-import _io
 import os
-import subprocess
 
 from consts import TEST_REPO
+from get_commits import get_commits
 
 
 def main():
     git_path_abs: str = "/usr/bin/git"
     file_path_abs: str = os.path.join(TEST_REPO, "src/main.py")
+
+    commits: dict[str, tuple[str, str, str]] = get_commits(TEST_REPO, "src/main.py")
+    for commit in commits.values():
+        print(commit)
 
 
 if __name__ == "__main__":
