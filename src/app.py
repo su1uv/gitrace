1213b2076c880592217c605c1a49fe7c9f9d86a2
su1uv <ortizemilio.dev@gmail.com>
2026-07-13 07:41:41 -0600

    [feat]: show commit preview with highlight (lexer)

--- src/app.py
+++ src/app.py
@@ -1,15 +1,18 @@
+import pygments
 from prompt_toolkit import Application
+from prompt_toolkit.formatted_text import PygmentsTokens
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.layout.containers import HSplit, Window
 from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.lexers.pygments import PygmentsLexer
 from prompt_toolkit.styles import Style
 from prompt_toolkit.styles.pygments import style_from_pygments_cls
 from prompt_toolkit.widgets import (
     Frame,
-    HorizontalLine,
     RadioList,
 )
+from pygments.lexers.python import PythonLexer
 from pygments.styles import get_style_by_name
 
 from .show_file import get_file_content
@@ -24,17 +27,15 @@ def exit_(event):
 
 def app(work_dir, file_path, commits):
     style: Style = style_from_pygments_cls(get_style_by_name("one-dark"))
-    # textarea = TextArea(
-    #     text=file_text,
-    #     lexer=PygmentsLexer(PythonLexer),
-    # )
-
     commit_list = RadioList(values=commits)
 
-    def get_commit_content() -> str:
+    def get_commit_content() -> PygmentsTokens:
         commit_id = commit_list.current_value
         commit_content = get_file_content("git", work_dir, file_path, commit_id)
-        return commit_content
+        tokens = list(pygments.lex(commit_content, lexer=PythonLexer()))
+        commit_tokens = PygmentsTokens(tokens)
+
+        return commit_tokens
 
     root_container = HSplit(
         [
